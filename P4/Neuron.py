eaafce89cbe1c61e40eb1b0c73268c1cc81681fd
stormjoannes <stormjoannes10@hotmail.com>
2021-03-10 12:31:23 +0100

    output neuron P4

--- P4/Neuron.py
+++ P4/Neuron.py
@@ -1,11 +1,14 @@
 import math
 
 class Neuron(object):
-    def __init__(self, weights: [float], bias: float) -> None:
+    def __init__(self, weights: [float], bias: float, learningRate: float) -> None:
         self.weights = weights
         self.bias = bias
         self.outp = 0
         self.errors = []
+        self.neuronError = 0
+        self.gradient = 0
+        self.learningRate = learningRate
 
     def activatieNeuron(self, inp: [float]):
         """
@@ -21,12 +24,36 @@ class Neuron(object):
     def sigmoid(self):
         self.outp = 1 / (1 + math.exp(-self.outp))
 
-    def setError(self):
+    def setError(self, target: float):
         """
-        Berekenen mean squared error.
+        Berekenen error.
         """
-        tweedeMacht = [self.errors[i] ** 2 for i in range(len(self.errors))]
-        return sum(tweedeMacht) / len(tweedeMacht)
+        afgelInp = self.outp * (1 - self.outp)
+        self.neuronError = afgelInp * -(target - self.outp)
+
+        return self.neuronError
+        # tweedeMacht = [self.errors[i] ** 2 for i in range(len(self.errors))]
+        # return sum(tweedeMacht) / len(tweedeMacht)
+
+    def setGradient(self, outpNeuron: float):
+        # deltaWeights = []
+        self.gradient = outpNeuron * self.neuronError
+
+        # for i in self.weights:
+        deltaWeights = self.learningRate * outpNeuron * self.neuronError
+        # deltaWeight = self.weights[i]
+        # deltaWeights.append(deltaWeight)
+
+        deltaBias = self.learningRate * self.neuronError
+        return deltaWeights, deltaBias
+
+    def update(self, input: [float], target: float, outpNeuron: float):
+        self.setError(target)
+        deltas = self.setGradient(outpNeuron)
+        for i in self.weights:
+            self.weights[i] = self.weights[i] - deltas[0]
+        self.bias = self.bias - deltas[1]
+
 
     def __str__(self) -> str:
         """
@@ -34,6 +61,6 @@ class Neuron(object):
         """
         return f'Uitvoer:  \nWeights: {self.weights} \nBias {self.bias} \nOutput: {self.outp}'
 
-testAndPort = Neuron(weights=[0.5, 0.5], bias=-1.5)
-output = []
+# testAndPort = Neuron(weights=[0.5, 0.5], bias=-1.5)
+# output = []
 print(round(testAndPort.activatieNeuron([0, 1])), 'ja')
